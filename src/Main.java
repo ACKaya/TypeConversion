44b313fd5674f3c1d8bb487ee287cd6e262c50dc
Anıl Cem Kaya <anlcemkaya@hotmail.com>
2023-07-07 18:56:31 +0300

    TypeConversion

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=10;
+        double b=a;
+        System.out.println(b);
+        double c=20;
+        int d=(int)c;
+        System.out.println(d);
+        String str=String.valueOf(d);
+        System.out.println(str);
+        int e=Integer.parseInt(str);
+        System.out.println(e);
+    }
+}
